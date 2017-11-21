ebc2bf4a3f3021733b1b56d1ad7f1da977b4ed79
cassshh <casvd@hotmail.com>
2017-11-21 20:27:07 +0100

    Select laptop

--- app/components/picker.component.js
+++ app/components/picker.component.js
@@ -3,10 +3,14 @@ angular.module('app')
     .component('picker', {
         templateUrl: 'components/picker.template.html',
         controller: function PickerController(LaptopFactory, $scope) {
+            
             LaptopFactory.getLaptops().then(laptops => {
-                console.log(laptops);
                 $scope.laptops = laptops;
             });
+
+            this.bash = (laptop) => {
+                $scope.laptop = laptop;
+            }
         }
 
     });
