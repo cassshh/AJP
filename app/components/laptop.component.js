ad17b6e29fd8b72caf1f8ef4f44bf878882b0e12
cassshh <casvd@hotmail.com>
2017-11-22 12:20:10 +0100

    Remove obsolete injection

--- app/components/laptop.component.js
+++ app/components/laptop.component.js
@@ -2,7 +2,7 @@
 angular.module('app')
     .component('laptop', {
         templateUrl: 'components/laptop.template.html',
-        controller: function LaptopController(LaptopFactory, MainService, $scope, $attrs) {
+        controller: function LaptopController(MainService, $scope, $attrs) {
 
             this.init = () => {
                 $scope.laptop = JSON.parse($attrs.laptop);
